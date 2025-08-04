52fb7cf2e50547a589822a35237c0febf12fbac9
“Danilo” <pamelasvb@gmail.com>
2025-08-04 00:26:21 -0300

    Corrige arquivo CORS

--- backend/src/main/java/com/advocacia/config/CorsConfig.java
+++ backend/src/main/java/com/advocacia/config/CorsConfig.java
@@ -12,8 +12,10 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // permite todos os endpoints
-                        .allowedOrigins("http://localhost:5173") // frontend
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://www.daniloaugustosantos.com.br")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
